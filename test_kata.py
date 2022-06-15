4d163ad690aa9f4c6f2363a466e932c359bee980
Stepan Martyushov <71297919+BuslaySt@users.noreply.github.com>
2022-06-15 22:06:11 +0300

    fix some deprecated letters

--- test_kata.py
+++ test_kata.py
@@ -1,14 +1,12 @@
 from kata import *
 import unittest
 
-
 class Test_Generate_Hashtag(unittest.TestCase):
     def tests(self):
-        # self.assertEqual(inc_dec.increment(3), 4)
-        self.assertEquals(seven(times(five())), 35)
-        self.assertEquals(four(plus(nine())), 13)
-        self.assertEquals(eight(minus(three())), 5)
-        self.assertEquals(six(divided_by(two())), 3)
+        self.assertEqual(seven(times(five())), 35)
+        self.assertEqual(four(plus(nine())), 13)
+        self.assertEqual(eight(minus(three())), 5)
+        self.assertEqual(six(divided_by(two())), 3)
 
 if __name__ == '__main__':
     unittest.main()
